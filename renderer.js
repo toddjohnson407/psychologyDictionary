91f1a863e8f4c30c67673c58eafef0e58c55c3ba
Todd Johnson <toddjohnson407@gmail.com>
2018-12-13 06:11:15 -0700

    all vocabluary definitions parsed from web

---
+++ renderer.js
@@ -0,0 +1,80 @@
+'use strict';
+
+const fetch = require('node-fetch');
+var remote = require('electron').remote;
+var fs = require('fs');
+// var electronDialog = remote.dialog;
+
+var words = [];
+var wordCount;
+var html;
+var termsArray = [];
+var allFiles = ["review1", "review2", "review3", "review4"]
+
+var splitString = function(data) {
+  // console.log(data);
+  var fileArray = data.split("  ");
+  // termsArray.push(fileArray);
+  console.log(fileArray.length);
+  // termsArray = [].concat(...termsArray);
+  // console.log(termsArray);
+  words = fileArray;
+  wordCount = words.length
+  console.log(words);
+  for (var i = 0; i < wordCount; i++) {
+    if (words[i].indexOf(" ") > 0) {
+      words[i] = words[i].split(" ").join("-");
+    }
+    getDefinitions(words[i]);
+  }
+}
+
+var parseTermFiles = function() {
+  for (var i = 0; i < 4; i++) {
+    var read = fs.readFile(`./psychologyTerms/${allFiles[i]}.txt`, `utf8`, function(err, data) {
+      splitString(data.toString());
+    });
+  }
+}
+
+var createTerm = function(word, definiton) {
+  var termList = document.getElementById("terms");
+  var term = document.createElement("LI");
+  term.innerText = `${word}: ${definiton.trim()}`;
+
+  termList.appendChild(term);
+}
+
+var getDefinitions = function(word) {
+  var firstChar = word.substring(0,1);
+  var url = `https://psychologydictionary.org/${word}`;
+
+  fetch(url)
+    .then(response => {
+      html = response.text();
+      // console.log(html);
+      return html;
+    })
+    .then(html => {
+      // console.log(html);
+
+      var parser = new DOMParser();
+      var doc = parser.parseFromString(html, "text/html");
+
+      var definiton = doc.getElementsByClassName("td-post-content");
+
+      if (definiton.length > 0) {
+        var text = definiton[0].textContent;
+
+        var textDefinitionSplit = text.split("}");
+        var textDefinition = textDefinitionSplit[3];
+        var betaDefintion = textDefinition.split("Related");
+        var finalDefinition = betaDefintion[0];
+        console.log(finalDefinition);
+        createTerm(word, finalDefinition);
+      }
+    })
+}
+
+parseTermFiles();
+
